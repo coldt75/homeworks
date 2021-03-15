3141d840e30c7eb486bf922ef8d64a74388ca1ce
coldt75 <timyrtretiak@gmail.com>
2021-03-15 23:56:52 +0200

    homework 2.1

--- home_work1/homework2.0/homework2.0/ViewController.swift
+++ home_work1/homework2.0/homework2.0/ViewController.swift
@@ -12,14 +12,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Password.layer.cornerRadius = 10
+        passwordTextField.layer.cornerRadius = 10
         login.layer.cornerRadius = 10
         Email.layer.cornerRadius = 10
     }
     
     @IBOutlet private var login: UIButton!
     @IBOutlet private var Email: UITextField!
-    @IBOutlet private var Password: UITextField!
-
+    @IBOutlet private var passwordTextField: UITextField!
+    
+    private var passwordTextIsHidden: Bool = true
+    
+    @IBAction func showPasswordButton(_ sender: UIButton) {
+        print("Show Password button got clicked")
+        passwordTextIsHidden = !passwordTextIsHidden
+        passwordTextField.isSecureTextEntry = passwordTextIsHidden
+    }
+    
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
+        print("Forgot Password button got clicked")
+    }
+    @IBAction func logInButtonClick(_ sender: UIButton) {
+        
+        print("Sign In button got clicked")
+    }
+    
+    @IBAction func continueAsButon(_ sender: UIButton) {
+        print("Continue as NAME button got clicked")
+    }
+    
+    @IBAction func signUpButton(_ sender: UIButton) {
+        print("Sign Up button got clicked")
+    }
+    
+    //Actions END
 }
+
+
 
