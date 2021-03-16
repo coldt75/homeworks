124c1bf8fba10601382a4a1a9b39b8eb0a6366a7
coldt75 <timyrtretiak@gmail.com>
2021-03-16 12:58:56 +0200

    home work 3

---
+++ homework_3/homework_2/ff.swift
@@ -0,0 +1,8 @@
+//
+//  ff.swift
+//  homework_2
+//
+//  Created by Timyr Tretiak on 15.03.2021.
+//
+
+import Foundation
---
+++ homework_3/homework_2Tests/homework_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  homework_2Tests.swift
+//  homework_2Tests
+//
+//  Created by Timyr Tretiak on 15.03.2021.
+//
+
+import XCTest
+@testable import homework_2
+
+class homework_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
