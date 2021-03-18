db49c8415bfe726dee19b00a56a77c60809b6fca
coldt75 <timyrtretiak@gmail.com>
2021-03-18 10:59:54 +0200

    homeworks

---
+++ homework_3/homework_2/BIgIcon_1.swift
@@ -0,0 +1,15 @@
+//
+//  BIgIcon_1.swift
+//  homework_2
+//
+//  Created by Timyr Tretiak on 16.03.2021.
+//
+
+import UIKit
+
+final class BIgIcon_1: UITableViewCell{
+    
+    
+}
+
+
--- homework_3/homework_2/ff.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ff.swift
-//  homework_2
-//
-//  Created by Timyr Tretiak on 15.03.2021.
-//
-
-import Foundation
